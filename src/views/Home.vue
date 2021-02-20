8978d6a1b9ca11f68da29844d4fee7f570264a23
cituk <programaciondyn3.mx@sandos.com>
2021-02-20 11:47:55 -0500

    Desarrollando un simple contador

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
- <HelloWorld></HelloWorld>
+  <div>
+    <SimpleCounter></SimpleCounter>
+    <HelloWorld></HelloWorld>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld'
+import SimpleCounter from '@/components/SimpleCounter'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld    
+    HelloWorld,
+    SimpleCounter   
   }
 }
 </script>
